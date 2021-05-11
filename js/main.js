b3d96254a70c0bae8a6f478e7d4cc209770bcd6f
Jonathan Sanderson <37599887+jonathan-sanderson@users.noreply.github.com>
2021-05-11 02:55:38 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,87 @@
+$(function () {
+
+	"use strict";
+
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	/* Contact-form
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$.validator.setDefaults({
+		submitHandler: function () {
+			alert("submitted!");
+		}
+	});
+
+	$(document).ready(function () {
+		$("#contact-form").validate({
+			rules: {
+				firstname: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				lastname: "required",
+				message: "required",
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				email: "Please enter a valid email address",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				message: "Please enter your Message",
+				agree: "Please accept our policy"
+			},
+			errorElement: "div",
+			errorPlacement: function (error, element) {
+				// Add the `help-block` class to the error element
+				error.addClass("help-block");
+
+				if (element.prop("type") === "checkbox") {
+					error.insertAfter(element.parent("input"));
+				} else {
+					error.insertAfter(element);
+				}
+			},
+			highlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
+			},
+			unhighlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+			}
+		});
+	});
+
+	function openNav() {
+		document.getElementById("myNav").style.width = "100%";
+		}
+	
+		function closeNav() {
+	   document.getElementById("myNav").style.width = "0%";
+	   }
+	
+	var acc = document.getElementsByClassName("faq");
+	var i;
+	
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+	
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+		  panel.style.display = "none";
+		} else {
+		  panel.style.display = "block";
+		}
+	  });
+	} 
+
+
+});
